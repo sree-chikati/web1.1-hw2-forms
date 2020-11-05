8d2cc4e58ab8be24ffc8fe300c6f54964ea267d7
sree-chikati <srechi453.fusdk12@gmail.com>
2020-11-04 19:44:17 -0800

    Part 1: Favorite Things

--- app.py
+++ app.py
@@ -36,12 +36,28 @@ def show_froyo_results():
 @app.route('/favorites')
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
-    pass
+    return """
+    <form action="/favorites_results" method="GET">
+        What is your favorite color? <br/>
+        <input type="text" name="color"><br/>
+
+        What is your favorite animal? <br/>
+        <input type="text" name="animal"><br/>
+
+        What is your favorite city? <br/>
+        <input type="text" name="city""><br/>
+
+        <input type="submit" value="Submit!">
+    </form>
+    """
 
 @app.route('/favorites_results')
 def favorites_results():
     """Shows the user a nice message using their form results."""
-    pass
+    users_fav_color = request.args.get('color')
+    users_fav_animal = request.args.get('animal')
+    users_fav_city = request.args.get('city')
+    return f'Wow, I didn\'t know {users_fav_color} {users_fav_animal} lived in {users_fav_city}!'
 
 @app.route('/secret_message')
 def secret_message():
