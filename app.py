318ca10ae9b9019025a5b1085be9f40689aaae5c
sree-chikati <srechi453.fusdk12@gmail.com>
2020-11-04 19:42:20 -0800

    Part 1: Fro-Yo Ordering

--- app.py
+++ app.py
@@ -17,12 +17,21 @@ def homepage():
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    pass
+    return """
+    <form action="/froyo_results" method="GET">
+        What is your favorite Fro-Yo flavor? <br/>
+        <input type="text" name="flavor"><br/>
+        What is your favorite Fro-Yo topping? <br/>
+        <input type="text" name="toppings"><br/>
+        <input type="submit" value="Submit!">
+    </form>
+    """
 
 @app.route('/froyo_results')
 def show_froyo_results():
-    """Shows the user what they ordered from the previous page."""
-    pass
+    users_froyo_flavor = request.args.get('flavor')
+    users_froyo_topping = request.args.get('toppings')
+    return f'You ordered {users_froyo_flavor} flavored Fro-Yo with toppings {users_froyo_topping}!'
 
 @app.route('/favorites')
 def favorites():
